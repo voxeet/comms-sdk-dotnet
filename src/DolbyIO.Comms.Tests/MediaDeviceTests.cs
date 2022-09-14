d6a4f32373a1010cc371b6634ca78942b4a22d06
Thomas Gourgues <thomas.gourgues@gmail.com>
2022-09-14 16:50:26 +0200

    Fixed typo in DolbyIO.Comms.Native and added MediaDevice call tests

--- src/DolbyIO.Comms.Tests/MediaDeviceTests.cs
+++ src/DolbyIO.Comms.Tests/MediaDeviceTests.cs
@@ -1,8 +1,16 @@
 using DolbyIO.Comms;
 namespace DolbyIO.Comms.Tests
 {
+    [Collection("Sdk")]
     public class MediaDeviceTests
     {
+        private SdkFixture _fixture;
+
+        public MediaDeviceTests(SdkFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
         [Fact]
         public void Test_AudioDevice_ShouldMashall()
         {
@@ -17,6 +25,18 @@ namespace DolbyIO.Comms.Tests
             Assert.Equal(uid, dest.Uid);
             Assert.Equal("dummy device", dest.Name);
             Assert.Equal(DeviceDirection.Output, dest.Direction);
+        }
+
+        [Fact]
+        public async void Test_MediaDevice_CanCallMethods()
+        {
+            AudioDevice device = new AudioDevice();
+
+            await _fixture.Sdk.MediaDevice.GetAudioDevices();
+            await _fixture.Sdk.MediaDevice.GetCurrentAudioInputDevice();
+            await _fixture.Sdk.MediaDevice.GetCurrentAudioOutputDevice();
+            await _fixture.Sdk.MediaDevice.SetPreferredAudioInputDevice(device);
+            await _fixture.Sdk.MediaDevice.SetPreferredAudioOutputDevice(device);
         }
     }
 }
