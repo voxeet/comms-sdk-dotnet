12707fc6205bb741cb0526b863f7c06b9f717185
Thomas Gourgues <2944786+4thlabs@users.noreply.github.com>
2022-12-29 17:40:29 +0100

    Added GetToken to DolbyIOManager (#98)
    * Added GetToken to DolbyIOManager
    
    * Changed the url to get the token
    
    * Fixed typo

--- scripts/DolbyIOManager.cs
+++ scripts/DolbyIOManager.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
 using UnityEngine;
 using Unity.VisualScripting;
 using DolbyIO.Comms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace DolbyIO.Comms.Unity
 {
@@ -23,6 +29,42 @@ namespace DolbyIO.Comms.Unity
         
         [Tooltip("Indicates if the DolbyIOManager should automatically close the session on application quit.")]
         public bool AutoCloseSession = true;
+
+        /// <summary>
+        /// For convenience during early development and prototyping, a method is provided for you to 
+        /// acquire a client access token directly from the application. However, please note Dolby does not recommend 
+        /// using this mechanism in the production software for (security best practices)[https://docs.dolby.io/communications-apis/docs/guides-client-authentication] reasons.
+        /// </summary>
+        /// <param name="key">The App key.</param>
+        /// <param name="secret">The App Secret.</param>
+        /// <returns>An asynchronous task containing the token.</returns>
+        public static async Task<string> GetToken(string key, string secret)
+        {
+            string result = "";
+
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, " https://session.voxeet.com/v1/oauth2/token");
+                var auth = $"{Uri.EscapeUriString(key)}:{Uri.EscapeUriString(secret)}";
+                
+                request.Headers.Authorization = new AuthenticationHeaderValue("Basic", $"{Convert.ToBase64String(Encoding.UTF8.GetBytes(auth))}");
+                request.Content = new FormUrlEncodedContent(new Dictionary<string, string> {{"grant_type", "client_credentials"}});
+
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var json = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString);
+
+                result = jsonString;
+                if (!json.TryGetValue("access_token", out result))
+                {
+                    throw new Exception("Unable to access json token");
+                }
+            }
+
+            return result;
+        }
 
         public static void QueueOnMainThread(Action a)
         {
