f3c9e66788663de2de039885aa2d78b355d0b8cc
Thomas Gourgues <2944786+4thlabs@users.noreply.github.com>
2023-01-03 18:52:23 +0100

    Unity package structure (#119)
    * Reworked package structure
    
    * Reworked the unity script for new structure

--- cmake/unity.cmake
+++ cmake/unity.cmake
@@ -1,33 +1,21 @@
 option(BUILD_UNITY "Build unity package" ON)
 
 if (BUILD_UNITY)
-    set(UNITY_RUNTIME_DIRECTORY "${CMAKE_BINARY_DIR}/unity")
+    if (WIN32)
+        set(UNITY_RUNTIME_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/unity/Plugins/win-x64/native")
+    elseif(APPLE)
+        set(UNITY_RUNTIME_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/unity/Plugins/osx-x64/native")
+    endif()
 
-    configure_file(cmake/package.json.in ${UNITY_RUNTIME_DIRECTORY}/package.json)
-
-    file(GLOB UNIT_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/scripts/Units/*.cs)
-
-    foreach(file IN LISTS UNIT_SOURCES)
-        list(APPEND COPY_COMMANDS 
-                COMMAND ${CMAKE_COMMAND} -E copy_if_different ${file} ${UNITY_RUNTIME_DIRECTORY}/Runtime/Units/
-            )
-    endforeach()
+    set(UNITY_ASSEMBLY_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/unity/Runtime/Assembly")
     
     add_custom_target(UnityPackage ALL
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:DolbyioComms::sdk> "${UNITY_RUNTIME_DIRECTORY}/Runtime/$<TARGET_FILE_NAME:DolbyioComms::sdk>"
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:DolbyioComms::media> "${UNITY_RUNTIME_DIRECTORY}/Runtime/$<TARGET_FILE_NAME:DolbyioComms::media>"
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:dvc> "${UNITY_RUNTIME_DIRECTORY}/Runtime/$<TARGET_FILE_NAME:dvc>"
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:dnr> "${UNITY_RUNTIME_DIRECTORY}/Runtime/$<TARGET_FILE_NAME:dnr>"
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:DolbyIO.Comms.Native> "${UNITY_RUNTIME_DIRECTORY}/Runtime/$<TARGET_FILE_NAME:DolbyIO.Comms.Native>"
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/DolbyIO.Comms.Sdk.dll "${UNITY_RUNTIME_DIRECTORY}/Runtime/DolbyIO.Comms.Sdk.dll"
-        
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/scripts/dolbyio.comms.asmdef "${UNITY_RUNTIME_DIRECTORY}/Runtime/dolbyio.comms.asmdef"
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/scripts/VectorExtension.cs "${UNITY_RUNTIME_DIRECTORY}/Runtime/Extensions/VectorExtension.cs"
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/scripts/DolbyIOManager.cs "${UNITY_RUNTIME_DIRECTORY}/Runtime/Components/DolbyIOManager.cs"
-        # Visual Scripting Units
-        COMMAND ${CMAKE_COMMAND} -E make_directory "${UNITY_RUNTIME_DIRECTORY}/Runtime/Units/"
-        
-        ${COPY_COMMANDS}
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:DolbyioComms::sdk> "${UNITY_RUNTIME_DIRECTORY}/$<TARGET_FILE_NAME:DolbyioComms::sdk>"
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:DolbyioComms::media> "${UNITY_RUNTIME_DIRECTORY}/$<TARGET_FILE_NAME:DolbyioComms::media>"
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:dvc> "${UNITY_RUNTIME_DIRECTORY}/$<TARGET_FILE_NAME:dvc>"
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:dnr> "${UNITY_RUNTIME_DIRECTORY}/$<TARGET_FILE_NAME:dnr>"
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:DolbyIO.Comms.Native> "${UNITY_RUNTIME_DIRECTORY}/$<TARGET_FILE_NAME:DolbyIO.Comms.Native>"
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/DolbyIO.Comms.Sdk.dll "${UNITY_ASSEMBLY_DIRECTORY}/DolbyIO.Comms.Sdk.dll"
 
         DEPENDS DolbyIO.Comms.Native DolbyIO.Comms.Sdk
     )
